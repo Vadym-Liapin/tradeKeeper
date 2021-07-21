f4de0b320e2afc18429da08c3a07a9d63f8b3fbd
Vadym <LiapinVadim@gmail.com>
2021-07-21 18:34:21 +0300

    Add files via upload

--- Oracle/Queries/basic.sql
+++ Oracle/Queries/basic.sql
@@ -9,7 +9,7 @@ BEGIN
 	&&SCHEMA.REPORTS_PKG.set_batch_ids(l_batch_ids);
     
     &&SCHEMA.REPORTS_PKG.set_orders_quantity(50);
-    &&SCHEMA.REPORTS_PKG.set_orders_prc(6);
+    &&SCHEMA.REPORTS_PKG.set_orders_prc(3);
 END;
 /
 /*
@@ -22,7 +22,26 @@ FROM 	dual;
 SELECT	ta.*
 FROM	&&SCHEMA.vw_trades_total ta;
 */
-/*
-SELECT	ta.*
-FROM	&&SCHEMA.vw_orders_total ta;
-*/
+
+SELECT	ta.batch,
+        ta.market,
+        ta.side,
+        CASE ta.side
+            WHEN 'ASK'
+            THEN MAX(ta.price) KEEP (DENSE_RANK FIRST ORDER BY ta.price ASC)
+            WHEN 'BID'
+            THEN MAX(ta.price) KEEP (DENSE_RANK FIRST ORDER BY ta.price DESC)
+        END AS price,
+        CASE ta.side
+            WHEN 'ASK'
+            THEN MAX(ta.quantity) KEEP (DENSE_RANK FIRST ORDER BY ta.price ASC)
+            WHEN 'BID'
+            THEN MAX(ta.quantity) KEEP (DENSE_RANK FIRST ORDER BY ta.price DESC)
+        END AS quantity
+FROM	&&SCHEMA.vw_orders_total ta
+GROUP BY    ta.batch,
+            ta.market,
+            ta.side
+ORDER BY    ta.batch,
+            ta.market,
+            CASE ta.side WHEN 'ASK' THEN 1 WHEN 'BID' THEN 2 END;
